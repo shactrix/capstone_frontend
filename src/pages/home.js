7c94a89c9e00c329e41abf0d43a827d3eb7fc464
Jon Reven <jonmountainboy@gmail.com>
2024-02-25 16:27:02 -0500

    folder structer and home.js

---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/home.css'
+
+const home = (props) => {
+    return (
+        <div className="home-page" >
+            <div className="transparent-box">
+                <div className="home-content">
+                    <h1>Welcome to RV WORLD</h1>
+                    <br />
+                    <h4>Are you a travel enthusiast? Or maybe want to just hit the open road?</h4>
+                    <h4>We got what your looking for!</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default home;
